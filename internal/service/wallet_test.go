2cee0a68987cbe8e8dc9f733e35f82bc91be4cf7
Andrew <andrew@example.com>
2026-03-10 18:40:55 +0300

    feat: add redis cache and service layer

--- internal/service/wallet_test.go
+++ internal/service/wallet_test.go
@@ -53,7 +53,11 @@ func (m *MockRepo) GetWallet(ctx context.Context, walletID string) (*postgresql.
 }
 func (m *MockRepo) CreateWallet(ctx context.Context, currency string) (string, error) { return "", nil }
 func (m *MockRepo) Transfer(ctx context.Context, fromID, toID string, amount float64) error {
-	return nil
+	// Эта строчка записывает в журнал testify, что метод был вызван с такими-то аргументами!
+	args := m.Called(ctx, fromID, toID, amount)
+
+	// Возвращаем ту ошибку, которую мы указали в тесте через .Return()
+	return args.Error(0)
 }
 
 // --- 3. САМ ТЕСТ ---
@@ -93,4 +97,41 @@ func TestWalletService_GetWallet_CacheHit(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, expectedWallet, resultWallet)
 	mockRepo.AssertNotCalled(t, "GetWallet", mock.Anything, testWalletID)
+}
+
+func TestWalletService_Transfer_Success(t *testing.T) {
+	// Подготовка (Arrange)
+	dummyLog := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	mockRepo := new(MockRepo)
+	mockCache := new(MockCache)
+	service := New(dummyLog, mockRepo, mockCache)
+
+	fromID := "wallet-1"
+	toID := "wallet-2"
+	amount := float64(100)
+
+	// ТВОЯ ЗАДАЧА: Срежиссировать сценарий для Моков
+
+	// 1. Скажи БД (mockRepo): когда вызовут твой метод "Transfer" с нашими fromID, toID и amount, верни nil (типа перевод успешен).
+	// Подсказка: mockRepo.On("Transfer", mock.Anything, fromID, toID, amount).Return(nil)
+	mockRepo.On("Transfer", mock.Anything, fromID, toID, amount).Return(nil)
+	// 2. Скажи Кэшу (mockCache): я ожидаю, что тебя попросят удалить кошелек fromID. Верни nil.
+	// Подсказка: mockCache.On("DeleteWallet", mock.Anything, fromID).Return(nil)
+	mockCache.On("DeleteWallet", mock.Anything, fromID).Return(nil)
+	// 3. Скажи Кэшу (mockCache): я ожидаю, что тебя попросят удалить кошелек toID. Верни nil.
+	mockCache.On("DeleteWallet", mock.Anything, toID).Return(nil)
+
+	// 4. Вызови тестируемый метод (Act): err := service.Transfer(...)
+
+	err := service.Transfer(context.Background(), fromID, toID, amount)
+	// 5. Проверь результат (Assert): убедись, что ошибка равна nil (assert.NoError)
+	assert.NoError(t, err)
+	// 6. ФИНАЛЬНАЯ ПРОВЕРКА (Ревизор)
+	// Эти две строчки проверяют, что сервис реально выполнил все условия из пунктов 1, 2 и 3!
+	// Если сервис "забыл" удалить чей-то кэш, тест упадет с криком "Вы обещали вызвать DeleteWallet, но не вызвали!".
+	// Напиши:
+	// mockRepo.AssertExpectations(t)
+	// mockCache.AssertExpectations(t)
+	mockRepo.AssertExpectations(t)
+	mockCache.AssertExpectations(t)
 }
